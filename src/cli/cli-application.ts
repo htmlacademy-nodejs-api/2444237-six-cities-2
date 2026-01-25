5c5bd11cf3fa3dcc08bd348e1b267aef00be1d65
Your Name <your.email@example.com>
2026-01-25 16:31:32 +0300

    Базовая реализация CLI приложения

---
+++ src/cli/cli-application.ts
@@ -0,0 +1,43 @@
+import { CommandParser } from "./command-parser.js";
+import { Command } from "./command.interfaсe.js";
+
+type CollectionCommand = Record<string, Command>;
+
+export class CliApplication {
+  constructor(private readonly defaultCommand: string = "--help") {}
+  private commands: CollectionCommand = {};
+
+  public registerCommands(commandList: Command[]) {
+    commandList.forEach((command) => {
+      if (Object.hasOwn(this.commands, command.getName())) {
+        throw new Error(`Command ${command.getName()} is already registered`);
+      }
+      this.commands[command.getName()] = command;
+    });
+  }
+
+  public getCommand(command: string): Command {
+    return this.commands[command] ?? this.getDefaultCommand();
+  }
+
+  public getDefaultCommand(): Command {
+    if (!this.defaultCommand) {
+      throw new Error("Default command is not defined");
+    }
+    return this.commands[this.defaultCommand];
+  }
+
+  public processParseCommand(args: string[]) {
+    const parsedArguments = CommandParser.parse(args);
+    if (Object.keys(parsedArguments).length === 0) {
+      const command = this.getDefaultCommand();
+      command.execute();
+      return;
+    }
+    console.log(parsedArguments);
+    const [commandName] = Object.keys(parsedArguments);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedArguments[commandName];
+    command.execute(...commandArguments);
+  }
+}
---
+++ src/cli/version.command.ts
@@ -0,0 +1,44 @@
+import { Command } from "./command.interfaсe.js";
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
+type PackageJsonConfig = {
+  version: string;
+};
+
+const isPackageJsonConfig = (value: unknown): value is PackageJsonConfig => {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    !Array.isArray(value) &&
+    Object.hasOwn(value, "version")
+  );
+};
+
+export class VersionCommand implements Command {
+  constructor(private filePath: string = "./package.json") {}
+
+  private readVersion(): string {
+    const jsonContent = readFileSync(resolve(this.filePath), "utf-8");
+    const importedContent: unknown = JSON.parse(jsonContent);
+
+    if (!isPackageJsonConfig(importedContent)) {
+      throw new Error("Failed to read package.json");
+    }
+
+    return importedContent.version;
+  }
+
+  getName(): string {
+    return "--version";
+  }
+
+  execute(..._parameters: string[]): void {
+    try {
+      const version = this.readVersion();
+      console.log(version);
+    } catch (error) {
+      throw new Error("The file package.json does not exist");
+    }
+  }
+}
